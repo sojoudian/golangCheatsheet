1511ddfedf93e2addbb2744b4a0e5b64656e816f
Maziar Sojoudian <maziar@Maziars-MacBook-Pro.local>
2022-05-01 20:03:54 -0400

    add array tests

--- test/pluralsight/GoGettingStarted/6/6.go
+++ test/pluralsight/GoGettingStarted/6/6.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 	slice := []int{1, 2, 3}
-
-	slice = append(slice, 4)
-
+	slice = append(slice, 4, 5, 6)
 	fmt.Println(slice)
+
+	s2 := slice[1:]
+	s3 := slice[:2]
+	s4 := slice[1:2]
+
+	fmt.Println(s2, s3, s4)
 
 }
